a0ea49a5c32779b7862a4bf678527c6b588dba6d
nitinkhare20 <145281870+nitinkhare20@users.noreply.github.com>
2023-09-19 07:52:19 -0700

    Add files via upload

---
+++ Lowest marks among 3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int P,R;
+    float CI;
+    scanf("%d%d",&P,&P);
+    CI=P*pow(1+R/100.0,2)-P;
+    printf("%.2f",CI);
+    
+    
+
+    return 0;
+}
+   
